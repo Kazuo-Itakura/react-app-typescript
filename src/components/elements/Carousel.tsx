e994c2cb4dd1a9921a8e6cad4c71533ba3aa2f85
itakura <no-reply@example.com>
2023-04-14 19:15:14 +0900

    カルーセルに< ■□□□ > を追加

--- src/components/elements/Carousel.tsx
+++ src/components/elements/Carousel.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// Carousel.tsx
+
+import React, { useState } from 'react';
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
@@ -7,21 +9,54 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ carousel_parts }) => {
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const handleSlideChange = (index: number) => {
+    setCurrentItem(index);
+  };
+
+  const handlePrevClick = () => {
+    setCurrentItem(currentItem === 0 ? carousel_parts.length - 1 : currentItem - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentItem(currentItem === carousel_parts.length - 1 ? 0 : currentItem + 1);
+  };
+
   return (
-    <ResponsiveCarousel
-      showArrows
-      infiniteLoop
-      showThumbs={false}
-      showStatus={false}
-      showIndicators={false}
-      selectedItem={0}
-      centerMode
-      centerSlidePercentage={50}
-    >
-      {carousel_parts.map((part, index) => (
-        <div key={index}>{part}</div>
-      ))}
-    </ResponsiveCarousel>
+    <div>
+      <ResponsiveCarousel
+        showArrows={false}
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        selectedItem={currentItem}
+        centerMode
+        centerSlidePercentage={50}
+        onChange={handleSlideChange}
+      >
+        {carousel_parts.map((part, index) => (
+          <div key={index}>{part}</div>
+        ))}
+      </ResponsiveCarousel>
+      <div className="flex items-center justify-center mt-4">
+        <button className="mr-4" onClick={handlePrevClick}>
+          {'<'}
+        </button>
+        {carousel_parts.map((_, index) => (
+          <span
+            key={index}
+            className={`mx-1 w-4 h-4 rounded-full ${
+              index === currentItem ? 'bg-black' : 'bg-gray-300'
+            }`}
+          ></span>
+        ))}
+        <button className="ml-4" onClick={handleNextClick}>
+          {'>'}
+        </button>
+      </div>
+    </div>
   );
 };
 
